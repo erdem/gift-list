42daa40405969e41a9e771d590c62b4309256d0b
erdem <github@erdemozkol.com>
2020-09-13 23:29:53 +0100

    added password for authentication flow

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,14 +1,16 @@
-from marshmallow import Schema, fields, validates, ValidationError, post_load
+from marshmallow import Schema, fields, validates, ValidationError, post_load, validates_schema
 
 from app.database import db
 from app.users.models import User, Token
+from app.users.utils import hash_md5
 
 
 class UserSchema(Schema):
     id = fields.Integer(dump_only=True)
     email = fields.Email(required=True)
-    first_name = fields.Str(required=True)
-    last_name = fields.Str(required=False)
+    password = fields.String(required=True, load_only=True)
+    first_name = fields.String(required=True)
+    last_name = fields.String(required=False)
     token = fields.Method('get_user_token', dump_only=True)
     created_at = fields.DateTime(dump_only=True)
 
@@ -34,7 +36,8 @@ class UserSchema(Schema):
             raise AttributeError(f'"instance" attribute not accessible, you must loaded the schema to access.')
 
     @post_load
-    def create(self, data, **kwargs):
+    def create_user(self, data, **kwargs):
+        data['password'] = hash_md5(data['password'])
         user = User(**data)
         token = Token()
         user.tokens.append(token)
@@ -46,3 +49,16 @@ class UserSchema(Schema):
 
 class AuthenticateUserSchema(Schema):
     email = fields.Email(required=True)
+    password = fields.String(required=True, load_only=True)
+
+    @validates_schema()
+    def validate_password(self, data, **kwargs):
+        is_password_valid = bool(
+            User.query.filter_by(
+                email=data['email'],
+                password=hash_md5(data['password'])
+            ).first()
+        )
+
+        if not is_password_valid:
+            raise ValidationError('Wrong password!', field_name='password')
